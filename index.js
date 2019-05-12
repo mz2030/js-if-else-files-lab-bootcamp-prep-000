dab5caddc25bab518b7e710342c101f03915460d
Mohammed Alshehri <eng.mohalshehri@gmail.com>
2019-05-12 20:53:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ if (name === "Susan") {
     console.log("The name is correct")
 }
 
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
